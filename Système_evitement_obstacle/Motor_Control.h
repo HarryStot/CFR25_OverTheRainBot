5423391edc5b37b7fda34da27fa44a0402e6b231
kinjoyce <kinjy.bialade@grenoble-inp.org>
2025-02-05 10:30:18 +0100

    Update Motor_Control.h

--- Système_evitement_obstacle/Motor_Control.h
+++ Système_evitement_obstacle/Motor_Control.h
@@ -2,16 +2,17 @@
 
 class Motor {
 private:
+  float u;
+  byte pwm;
   byte enca;
   byte encb;
-  byte in1;
-  byte in2;
+  byte dir;
 
 public:
   volatile int pos;
 
   Motor() {}  //Ne pas utiliser
-  Motor(byte enca, byte encb, byte in1, byte in2);
+  Motor(byte enca, byte encb, byte pwm, byte dir);
 
   void init();
   void setMotorSpeed(int phi);
