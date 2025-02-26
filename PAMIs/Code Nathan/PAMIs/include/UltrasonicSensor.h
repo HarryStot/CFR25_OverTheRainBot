435880838322875b82072eb0bdaf43d22f6c6aae
Nathan <bourguignon.nathan@gmail.com>
2025-02-26 13:52:40 +0100

    Fix issuer

--- PAMIs/Code Nathan/PAMIs/include/UltrasonicSensor.h
+++ PAMIs/Code Nathan/PAMIs/include/UltrasonicSensor.h
@@ -8,12 +8,6 @@ class UltrasonicSensor : public Component {
 private:
     int trigPin;
     int echoPin;
-<<<<<<< Updated upstream
-    double distance;
-
-public:
-    UltrasonicSensor(String name, int trig, int echo) : Component(name), trigPin(trig), echoPin(echo), distance(0) {
-=======
     double distanceCm;
     unsigned long maxTimeoutMicroSec = 0;
     const double speedOfSoundInCmPerMicroSec = 0.034;
@@ -21,30 +15,19 @@ public:
 
 public:
     UltrasonicSensor(String name, int trig, int echo) : Component(name), trigPin(trig), echoPin(echo), maxDistanceCm(0) {
->>>>>>> Stashed changes
         pinMode(trigPin, OUTPUT);
         pinMode(echoPin, INPUT);
     }
 
     void update() override {
-<<<<<<< Updated upstream
-=======
         unsigned long maxDistanceDurationMicroSec;
 
->>>>>>> Stashed changes
         digitalWrite(trigPin, LOW);
         delayMicroseconds(2);
         digitalWrite(trigPin, HIGH);
         delayMicroseconds(10);
         digitalWrite(trigPin, LOW);
 
-<<<<<<< Updated upstream
-        long duration = pulseIn(echoPin, HIGH);
-        distance = duration * 0.034 / 2;  // Convert to cm
-    }
-
-    double getDistance() { return distance; }
-=======
         // Compute max delay based on max distance with 25% margin in microseconds
         maxDistanceDurationMicroSec = 2.5 * maxDistanceCm / speedOfSoundInCmPerMicroSec;
         if (maxTimeoutMicroSec > 0) {
@@ -58,7 +41,6 @@ public:
     }
 
     double getDistance() { return distanceCm; }
->>>>>>> Stashed changes
 };
 
 #endif // ULTRASONICSENSOR_H
