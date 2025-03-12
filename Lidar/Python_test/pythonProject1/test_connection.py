4f0a421a265decad221688188a74f8c2fac9592b
Nathan <bourguignon.nathan@gmail.com>
2025-03-12 12:07:51 +0100

    Add test_scan.py

--- Lidar/Python_test/pythonProject1/test_connection.py
+++ Lidar/Python_test/pythonProject1/test_connection.py
@@ -1,26 +1,24 @@
 from pyrplidar import PyRPlidar
 
-lidar = PyRPlidar()
-lidar.connect(port="COM3", baudrate=256000, timeout=3)
-# Linux   : "/dev/ttyUSB0"
-# MacOS   : "/dev/cu.SLAB_USBtoUART"
-# Windows : "COM5"
+if __name__ == '__main__':
+    lidar = PyRPlidar()
+    lidar.connect(port="COM3", baudrate=256000, timeout=3)
+    # Linux   : "/dev/ttyUSB0"
+    # MacOS   : "/dev/cu.SLAB_USBtoUART"
+    # Windows : "COM5"
 
+    info = lidar.get_info()
+    print("info :", info)
 
-info = lidar.get_info()
-print("info :", info)
+    health = lidar.get_health()
+    print("health :", health)
 
-health = lidar.get_health()
-print("health :", health)
+    samplerate = lidar.get_samplerate()
+    print("samplerate :", samplerate)
 
-samplerate = lidar.get_samplerate()
-print("samplerate :", samplerate)
+    scan_modes = lidar.get_scan_modes()
+    print("scan modes :")
+    for scan_mode in scan_modes:
+        print(scan_mode)
 
-
-scan_modes = lidar.get_scan_modes()
-print("scan modes :")
-for scan_mode in scan_modes:
-    print(scan_mode)
-
-
-lidar.disconnect()
+    lidar.disconnect()
