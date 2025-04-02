29d2faa1d5cf3337606242fb11a81b5d1f7ba43f
nem0zz <nemo@clubcapelli.com>
2025-04-02 14:34:45 +0200

    Ajout récup/traitement des donnees ultrasons

--- Système_evitement_obstacle/Raspberry_Robot_Principal/machine_etat.py
+++ Système_evitement_obstacle/Raspberry_Robot_Principal/machine_etat.py
@@ -1,12 +1,18 @@
 import serial
 import time
 import math
+import threading
 
-ser_arduino1 = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) # Arduino1 = celle qui gère les moteurs/odométrie
+ser_arduino1 = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # Arduino1 = gestion des moteurs/odométrie
+ser_arduino2 = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)  # Arduino2 = ultrasons/servo-moteurs
 
-# parametres physiques du robot
-L = 0.245 / 2 # écart entre les 2 roues
-r = 0.065 / 2 # rayon d'une roue
+# paramètres physiques du robot
+L = 0.245 / 2  # écart entre les 2 roues
+r = 0.065 / 2  # rayon d'une roue
+beta = 45 * 2*math.pi/360 # angle capteur ultrason/sol
+
+# Variables initialisées pour les rendre globales
+x, y, theta = 0.0, 0.0, 0.0
 
 class Robot:
     def __init__(self):
@@ -14,8 +20,8 @@ class Robot:
         self.x_goal = [0, 0, 2, 0]
         self.y_goal = [0, 1, 1, 0]
         self.i_goal = 0
-        self.K1 = 300 # PID
-        self.eps = 0.1 # small threshold for determining when the robot has reached a goal
+        self.K1 = 2  # Gain
+        self.eps = 0.1  # seuil pour atteindre le but
 
     def send_speed_arduino1(self, v, w):
         phiR = (v + w * L) / r
@@ -43,18 +49,40 @@ class Robot:
         elif self.state == "STOP":
             self.send_speed_arduino1(0, 0)
 
-robot = Robot() #initialise __init__
+robot = Robot()
+
+#-----ULTRASONS-----
+def thread_serial_arduino2():
+    while True:
+        try:
+            data = ser_arduino2.readline().decode().strip()
+            if data.startswith("us"):
+                prefix, sensor_id, mesure = data.split(',')
+                sensor_id = int(sensor_id)
+                mesure = float(mesure)
+                # Todo : Rajouter la différence entre capteurs
+                x_obstacle = math.cos(theta)*mesure/math.sin(beta) 
+                y_obstacle = math.sin(theta)*mesure/math.sin(beta)
+                # Todo : rajouter l'obstacle dans la liste d'obstacle
+            time.sleep(0.1)
+        except Exception as e:
+            print(f"Erreur dans le thread série: {e}")
+            break
+thread_ultrason = threading.Thread(target=thread_serial_arduino2)
+thread_ultrason.daemon = True
+thread_ultrason.start()
+#-----------------
 
 while True:
     try:
-        #Demande les donnees x,y,theta, puis les met à jour en appelant update_state
         ser_arduino1.write(b"\n")  # Demander les données
         data = ser_arduino1.readline().decode().strip()
         if data:
-            x, y, theta = map(float, data.split(',')) #ex : convertit "1.0, 1.2, 3.6" en [1.0, 1.2, 3.6]
+            x, y, theta = map(float, data.split(','))
             robot.update_state(x, y, theta)
         time.sleep(0.1)
 
     except KeyboardInterrupt:
         ser_arduino1.close()
+        ser_arduino2.close()
         break
